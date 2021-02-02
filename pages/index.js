170260a72a1b44ddf857a60c1ba87ca2895f8a3c
Ricardo Michelangelo de Sa <ricardo.mdesa@gmail.com>
2021-02-01 21:45:07 -0300

    aula 5

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
-import db from '../db.json';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
+import Head from 'next/head';
 
+import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
@@ -10,6 +12,7 @@ import Footer from '../src/components/Footer'
 import Button from '../src/components/Button'
 import Input from '../src/components/Input'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Link from '../src/components/Link'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -35,14 +38,29 @@ export default function Home() {
 
   return (
     <QuizBackground backgroundImage={db.bg}>
+
+      <Head>
+        <title>
+          AluraQuiz - {db.title}
+        </title>
+      </Head>
+
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget 
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
             <form onSubmit={(e) => {
               e.preventDefault();
               router.push(`/quiz?name=${name}`);
@@ -50,7 +68,7 @@ export default function Home() {
               <Input 
               name = "nomeDoUser"
               onChange = {(e) => setName(e.target.value)}
-              placeholder="Digite seu nome"
+              placeholder="Diz ai seu nome"
               value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
@@ -60,14 +78,50 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Content>
             <h1>Quizes da Galera</h1>
+            <ul>
+              {db.external.map((link, index) => {
+                const [projName, githubUser] = link
+                  .replace(/\//g,'')
+                  .replace('https:','')
+                  .replace('.vercel.app','')
+                  .split('.');
 
-            <p>lorem ipsum dolor sit amet...</p>
+                return (
+                <li key={link}>
+                  <Widget.Topic 
+                    as={Link}
+                    href={`/quiz/${projName}___${githubUser}`}
+                  >
+                      {`${githubUser}/${projName}`}
+                  </Widget.Topic>
+                </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
-        <Footer />
+        <Footer 
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/ricardomdesa" />
     </QuizBackground>
